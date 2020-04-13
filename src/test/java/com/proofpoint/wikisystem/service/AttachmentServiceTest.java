cbe76a92c6ef621947a3e12a506e14630ec2a19f
Shekhar Bhosale <bhosale@adobe.com>
2020-04-12 18:47:26 -0600

    sample test code

---
+++ src/test/java/com/proofpoint/wikisystem/service/AttachmentServiceTest.java
@@ -0,0 +1,56 @@
+package com.proofpoint.wikisystem.service;
+
+import com.proofpoint.wikisystem.model.Attachment;
+import com.proofpoint.wikisystem.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+public class AttachmentServiceTest {
+
+    @InjectMocks
+    AttachmentService attachmentService;
+
+    @Mock
+    Map<String, Attachment> attachments;
+
+    @Mock
+    User user;
+
+    @BeforeEach
+    void setup() throws Exception{
+        MockitoAnnotations.initMocks(this);
+        attachments = new HashMap<>();
+    }
+
+    @Test
+    final void testCreate(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    final void testRead(){
+       Attachment attachment = Attachment.Builder
+                                .newInstance()
+                                .withFilename("Sample.txt")
+                                .withContents("Avsdvdsaadfcdfc")
+                                .withOwner(user)
+                                .build();
+
+       when(attachments.get(anyString())).thenReturn(attachment);
+
+        Attachment attachmentv2 = attachmentService.read(anyString());
+        assertNotNull(attachmentv2);
+        assertEquals("Sample.txt",attachmentv2.getFilename());
+
+    }
+}
